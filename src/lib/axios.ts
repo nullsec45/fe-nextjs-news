c3a1292eda62dee12b02cf3771179ba43594d4ab
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-18 16:10:57 +0700

    feat(lib) : add setup interceptor axios.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+import { error } from "console";
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+import { getCookie } from "cookies-next";
 
 const baseUrl=process.env.NEXT_PUBLIC_API_URL;
 
@@ -9,5 +13,38 @@ const axiosInstance=axios.create({
         'Content-Type':'application/json'
     }
 })
+
+export function setupInterceptor(){
+    const router=useRouter();
+
+    axiosInstance.interceptors.request.use(
+        async (config) => {
+            if (typeof window !== 'undefined'){
+                const token=getCookie('accessToken');
+
+                if(token){
+                    config.headers.Authorization=`Bearer ${token}`;
+                }
+            }
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
+    axiosInstance.interceptors.response.use(
+        (response) => response,
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                deleteCookie('accessToken');
+
+                router.push("/login");
+            }
+
+            return Promise.reject(error);
+        }
+    )
+}
 
 export default axiosInstance
